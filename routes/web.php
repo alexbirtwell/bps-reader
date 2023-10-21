6644cd74d0692c7f7d83072aba01429678c641dd
Alex Birtwell <alex@alexbirtwell.co.uk>
2023-10-21 09:02:07 +0100

    concept done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Reading;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\ErrorHandler\Debug;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/in/{key}', function (Request $request, $key) {
+Route::middleware(['cors'])->post('/in/{key}', function (Request $request, $key) {
     if ($key !== env('SECURE_KEY')) {
         return response()->json(['error' => 'Invalid API key'], 401);
     }
-    $body = $request->get('body');
-    \App\Models\Reading::create([
-        'raw' => $body
-    ]);
-    return view('welcome');
+
+    $data = explode($request->get('body'), '|');
+    foreach($data as $d) {
+        $reading = json_decode($d);
+        $created[] = Reading::create([
+            'raw' => $d,
+            'source_id' => data_get($reading, 'sourceId', ''),
+            'value' => data_get($reading, 'value', ''),
+            'sourceTimestamp' => data_get($reading, 'sourceTimestamp', null),
+        ]);
+    }
+
+    return response()->json(['success' => true, 'created' => collect($created)->pluck('id')->toArray()]);
 });
