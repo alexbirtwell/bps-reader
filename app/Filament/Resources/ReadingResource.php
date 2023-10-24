ffe1f68ae81adb2f94cc84beda4d289f30d00f07
Alex Birtwell <alex@alexbirtwell.co.uk>
2023-10-24 22:15:05 +0100

    Add resource

---
+++ app/Filament/Resources/ReadingResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ReadingResource\Pages;
+use App\Filament\Resources\ReadingResource\RelationManagers;
+use App\Models\Reading;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ReadingResource extends Resource
+{
+    protected static ?string $model = Reading::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-meter';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListReadings::route('/'),
+        ];
+    }
+}
